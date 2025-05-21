255fe7d2f498c564b84c7d56c931553463fb1b95
Muhammad Ravat <m0ravat736@gmail.com>
2025-05-21 17:57:52 +0100

    enabled cors on backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
+
+server.use(cors({
+  origin: 'https://moravat.me'
+}));
 
 server.use(middlewares);
 server.use(router);
+
 server.listen(process.env.PORT || 3001, () => {
   console.log('JSON Server is running');
 });
+
